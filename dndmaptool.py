bccaf6ca0d1080496509ad97cf0ba2c1c73384ff
Koeltekn <barletta.andrea@outlook.com>
2022-05-03 18:25:44 +0200

    Last commit o mi ammazzo

--- dndmaptool.py
+++ dndmaptool.py
@@ -4,20 +4,20 @@ import math
 import os
 
 A3=(11,16)
-A3mm=(297,420)
-A4=(5,8)
-A4mm=(210,297)
+A3margin=(0.35,0.25)
+A4=(8,11)
+A4margin=(0.15,0.35)
 
 print("Choose format (A3 / A4): ",end='')
 paperformatinput=input();
 if paperformatinput=="A3":
     paperformat=A3
-    papermm=A3mm
-    pdf = FPDF('P', 'mm', 'A3')
+    papermargin=A3margin
+    pdf = FPDF('P', 'in', 'A3')
 elif paperformatinput=="A4":
     paperformat=A4
-    papermm=A4mm
-    pdf = FPDF('P', 'mm', 'A4')
+    papermargin=A4margin
+    pdf = FPDF('P', 'in', 'A4')
 else:
     print("Wrong paper format")
     exit()
@@ -38,6 +38,6 @@ for i in range(0,math.ceil(rows/paperformat[1])):
         img_cropped=scalata.crop((j*dpi*paperformat[0],i*dpi*paperformat[1],(j+1)*dpi*paperformat[0],(i+1)*dpi*paperformat[1]))
         img_cropped.save("cropped/cropped"+str(j)+"_"+str(i)+".jpg")
         pdf.add_page()
-        pdf.image("cropped/cropped"+str(j)+"_"+str(i)+".jpg",0,0,papermm[0],papermm[1])
+        pdf.image("cropped/cropped"+str(j)+"_"+str(i)+".jpg",papermargin[0],papermargin[1],paperformat[0],paperformat[1])
 
 pdf.output("cropped.pdf", "F")
