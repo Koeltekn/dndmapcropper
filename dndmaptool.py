9fb3a743588882e3c58e6e53bae1c745df435fd1
Koeltekn <barletta.andrea@outlook.com>
2022-05-03 17:45:10 +0200

    Added paper format

--- dndmaptool.py
+++ dndmaptool.py
@@ -2,6 +2,26 @@ from PIL import Image
 from fpdf import FPDF
 import math
 import os
+
+A3=(11,16)
+A3mm=(297,420)
+A4=(5,8)
+A4mm=(210,297)
+
+print("Choose format (A3 / A4): ",end='')
+paperformatinput=input();
+if paperformatinput=="A3":
+    paperformat=A3
+    papermm=A3mm
+    pdf = FPDF('P', 'mm', 'A3')
+elif paperformatinput=="A4":
+    paperformat=A4
+    papermm=A4mm
+    pdf = FPDF('P', 'mm', 'A4')
+else:
+    print("Wrong paper format")
+    exit()
+
 print("Rows: ",end='')
 rows = int(input())
 print("Columns: ",end='')
@@ -12,12 +32,12 @@ dpi=img.info["dpi"][0]
 width, height=img.size
 scalata=img.resize((dpi*columns,dpi*rows))
 os.mkdir("cropped")
-pdf = FPDF('P', 'mm', 'A3')
-for i in range(0,math.ceil(rows/16)):
-    for j in range(0,math.ceil(columns/11)):
-        img_cropped=scalata.crop((j*dpi*11,i*dpi*16,(j+1)*dpi*11,(i+1)*dpi*16))
+
+for i in range(0,math.ceil(rows/paperformat[1])):
+    for j in range(0,math.ceil(columns/paperformat[0])):
+        img_cropped=scalata.crop((j*dpi*paperformat[0],i*dpi*paperformat[1],(j+1)*dpi*paperformat[0],(i+1)*dpi*paperformat[1]))
         img_cropped.save("cropped/cropped"+str(j)+"_"+str(i)+".jpg")
         pdf.add_page()
-        pdf.image("cropped/cropped"+str(j)+"_"+str(i)+".jpg",0,0,297,420)
+        pdf.image("cropped/cropped"+str(j)+"_"+str(i)+".jpg",0,0,papermm[0],papermm[1])
 
 pdf.output("cropped.pdf", "F")
