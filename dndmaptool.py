4693be0bd9eb939170256c8afd6ed7c04fac4ed0
Koeltekn <barletta.andrea@outlook.com>
2022-05-04 11:22:56 +0200

    Added more supported files, refactored code, fixed couple of bugs

--- dndmaptool.py
+++ dndmaptool.py
@@ -1,53 +1,127 @@
+from asyncio import wait_for
 from PIL import Image
 from fpdf import FPDF
 import math
 import os
+import shutil
 
-A3=(11,16)
-A3margin=(0.35,0.25)
-A4=(8,11)
-A4margin=(0.15,0.35)
+folder_name = "tiles/"
+pdf_name = "tiles"
+A3 = (11, 16)
+A3margin = (0.35, 0.25)
+A4 = (8, 11)
+A4margin = (0.15, 0.35)
 
-print("Choose format (A3 / A4): ",end='')
-paperformatinput=input();
-if paperformatinput=="A3":
-    paperformat=A3
-    papermargin=A3margin
-    pdf = FPDF('P', 'in', 'A3')
-elif paperformatinput=="A4":
-    paperformat=A4
-    papermargin=A4margin
-    pdf = FPDF('P', 'in', 'A4')
-else:
-    print("Wrong paper format")
+
+def wait_for_key():
+    print("Press any key to exit")
+    input()
     exit()
 
-print("Rows: ",end='')
+
+def get_map():
+    map_path = os.path.join(os.path.dirname(__file__), "map.jpg")
+    try:
+        img = Image.open(map_path)
+    except:
+        map_path = os.path.join(os.path.dirname(__file__), "map.jpeg")
+        try:
+            img = Image.open(map_path)
+        except:
+            map_path = os.path.join(os.path.dirname(__file__), "map.png")
+            try:
+                img = Image.open(map_path)
+            except:
+                map_path = os.path.join(os.path.dirname(__file__), "map.webp")
+                try:
+                    img = Image.open(map_path)
+                except:
+                    print('Map file could not be found, rename the file as "map" (supported file types png, jpg, jpeg, webp)')
+                    wait_for_key()
+
+    return img
+
+
+def create_folder():
+    try:
+        os.mkdir(folder_name)
+    except FileExistsError as error:
+        shutil.rmtree(folder_name)
+        os.mkdir(folder_name)
+
+
+def get_image_dpi(image):
+    try:
+        dpi = int(image.info["dpi"][0])
+    except:
+        print("Image dpi could not be obtained, type the image DPI: ", end="")
+        dpi = int(input())
+
+    return dpi
+
+
+def get_paper_format():
+    paperformatinput = input()
+    if paperformatinput == "A3":
+        paperformat = A3
+        papermargin = A3margin
+        pdf = FPDF("P", "in", "A3")
+    elif paperformatinput == "A4":
+        paperformat = A4
+        papermargin = A4margin
+        pdf = FPDF("P", "in", "A4")
+    else:
+        paperformat = ""
+        papermargin = ""
+        pdf = ""
+
+    return (paperformat, papermargin, pdf)
+
+
+map_img = get_map()
+dpi = get_image_dpi(map_img)
+create_folder()
+
+print("Number of rows: ", end="")
 rows = int(input())
-print("Columns: ",end='')
+print("Number of columns: ", end="")
 columns = int(input())
-mydata_path = os.path.join(os.path.dirname(__file__), "map.jpg")
-img = Image.open(mydata_path)
-try:
-    dpi=int(img.info["dpi"][0])
-except:
-    print("DPI: ",end='')
-    dpi=int(input())
 
-scaled=img.resize((dpi*columns,dpi*rows))
-width, height=scaled.size
-os.mkdir("cropped")
+print("Choose output format (A3 / A4): ", end="")
+paperformat, papermargin, pdf = get_paper_format()
+while(paperformat==""):
+    print("Invalid paper format, choose either A4 or A3")
+    paperformat, papermargin, pdf = get_paper_format()
 
-fullwidth=math.floor(width/(paperformat[0]))*columns
-fullheight=math.floor(height/(paperformat[1]))*rows
-scaled_bg=Image.new('RGB',(fullwidth,fullheight),(255,255,255))
-scaled_bg.paste(scaled,(0,0))
 
-for i in range(0,math.ceil(rows/paperformat[1])):
-    for j in range(0,math.ceil(columns/paperformat[0])):
-        img_cropped=scaled_bg.crop((j*dpi*paperformat[0],i*dpi*paperformat[1],(j+1)*dpi*paperformat[0],(i+1)*dpi*paperformat[1]))
-        img_cropped.save("cropped/cropped"+str(j)+"_"+str(i)+".jpg")
+# Resize image
+scaled_img = map_img.resize((dpi * columns, dpi * rows))
+width, height = scaled_img.size
+
+# Add white padding
+fullwidth = (math.ceil(width/(paperformat[0]*dpi)))*(columns*dpi)
+fullheight = (math.ceil(height/(paperformat[1]*dpi)))*(rows*dpi)
+scaled_padded_img = Image.new("RGB", (fullwidth, fullheight), (255, 255, 255))
+scaled_padded_img.paste(scaled_img, (0, 0))
+
+for i in range(0, math.ceil(rows / paperformat[1])):
+    for j in range(0, math.ceil(columns / paperformat[0])):
+        img_cropped = scaled_padded_img.crop(
+            (
+                j * dpi * paperformat[0],
+                i * dpi * paperformat[1],
+                (j + 1) * dpi * paperformat[0],
+                (i + 1) * dpi * paperformat[1],
+            )
+        )
+        img_cropped.save(folder_name + str(j) + "_" + str(i) + ".jpg")
         pdf.add_page()
-        pdf.image("cropped/cropped"+str(j)+"_"+str(i)+".jpg",papermargin[0],papermargin[1],paperformat[0],paperformat[1])
+        pdf.image(
+            folder_name + str(j) + "_" + str(i) + ".jpg",
+            papermargin[0],
+            papermargin[1],
+            paperformat[0],
+            paperformat[1],
+        )
 
-pdf.output("cropped.pdf", "F")
+pdf.output(pdf_name + ".pdf", "F")
